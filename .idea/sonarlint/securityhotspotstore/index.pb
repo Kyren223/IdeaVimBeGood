cb517af151670cb0db9dd23fe158938dfa05bd34
Kyren223 <ulmliad223@gmail.com>
2024-04-06 12:51:25 +0300

    Made the X minigame, it works really nicely Make sure to add this to your .ideavimrc file: `nnoremap x x:w<cr>:action Synchronize<cr>` When X is pressed, write to file (:w<cr>) and then call IntelliJ's Synchronize action to "load" the saved file from disk

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,11 @@
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 S
 #src/main/java/me/kyren223/Main.java,d\d\dd1b44c12e304ba84a62da79bfb19c37db49e695
+8
+test.txt,4\b\4b6fcb2d521ef0fd442a5301e7932d16cc9f375a
+Z
+*src/main/java/me/kyren223/FileWatcher.java,2\2\22a13f5a380eafb8bb770ce57f83a917f64ad014
+Å
+Q.idea/sonarlint/securityhotspotstore/4/b/4b6fcb2d521ef0fd442a5301e7932d16cc9f375a,f\0\f0976c4b0fff58d92a34062cd715a9c961b5a0e1
+w
+G.idea/sonarlint/issuestore/4/b/4b6fcb2d521ef0fd442a5301e7932d16cc9f375a,4\f\4f0d1543b2303e64d547e92ce01b3b2f5d47d49f
--- src/main/java/me/kyren223/Main.java
+++ src/main/java/me/kyren223/Main.java
@@ -1,7 +1,88 @@
 package me.kyren223;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+import java.util.function.UnaryOperator;
+
 public class Main {
+    public static final String PATH = "test.txt";
+    public static final int LINES = 20;
+    public static final int COLUMNS = 50;
+    public static final String INITIAL_CONTENT = generateInitialContent(LINES, COLUMNS);
+    public static final int SLEEP_DURATION_MILLIS = 10;
+    public static final int DURATION_IN_SECONDS = 10;
+    public static final int TIMES_TO_UPDATE = DURATION_IN_SECONDS * 1000 / SLEEP_DURATION_MILLIS;
+    private static Random random;
+    private static int score = 0;
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Path path = Paths.get(PATH);
+        try {
+            createFileIfNotExists();
+            Files.writeString(path, INITIAL_CONTENT);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        
+        UnaryOperator<String> updateFunction = currentString -> {
+            if (currentString.contains("X")) return currentString;
+            score++;
+            return generateInitialContent(LINES, COLUMNS);
+        };
+        
+        try {
+            monitorFileForUpdates(updateFunction);
+            Files.writeString(path, "Game over! Your score is: " + score);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        
     }
-}
+    
+    private static String generateInitialContent(int lines, int columns) {
+        if (random == null) {
+            random = new Random();
+        }
+        int randomX = random.nextInt(columns);
+        int randomY = random.nextInt(lines);
+        
+        StringBuilder sb = new StringBuilder();
+        for (int y = 0; y < lines; y++) {
+            for (int x = 0; x < columns; x++) {
+                sb.append((x == randomX && y == randomY) ? "X" : " ");
+            }
+            sb.append("\n");
+        }
+        
+        return sb.toString();
+    }
+    
+    private static void createFileIfNotExists() throws IOException {
+        Path filePath = Paths.get(PATH);
+        if (!Files.exists(filePath)) {
+            Files.createFile(filePath);
+        }
+    }
+    
+    private static void monitorFileForUpdates(UnaryOperator<String> updateFunction)
+            throws IOException, InterruptedException {
+        Path filePath = Paths.get(PATH);
+        String currentString = Files.readString(filePath);
+        
+        boolean started = false;
+        
+        for (int i = 0; i < TIMES_TO_UPDATE; i += started ? 1 : 0) {
+            Thread.sleep(SLEEP_DURATION_MILLIS);
+            
+            String newString = Files.readString(filePath);
+            if (!currentString.equals(newString)) {
+                if (!started) started = true;
+                currentString = updateFunction.apply(newString);
+                Files.writeString(filePath, currentString);
+            }
+        }
+    }}
